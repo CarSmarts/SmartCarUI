9b82d5e6e40ee84088dcbff77dca1b7dc864323b
Jackson Smith <rjsmith1999@gmail.com>
2020-05-26 20:03:53 -0400

    Add Picker.swift

---
+++ Sources/SmartCarUI/Picker.swift
@@ -0,0 +1,92 @@
+//
+//  Picker.swift
+//  SmartCarUI
+//
+//  Created by Robert Smith on 5/26/20.
+//  Copyright © 2020 Robert Smith. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public class PickerObject: NSObject, ObservableObject, UIDocumentPickerDelegate {
+    @Published public var isPresented: Bool = false {
+        didSet {
+            if isPresented && rootVC.presentedViewController == nil {
+                rootVC.present(picker, animated: true, completion: nil)
+            } else if !isPresented && rootVC.presentedViewController != nil {
+                rootVC.dismiss(animated: true)
+            }
+        }
+    }
+    
+    private var picker: UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(documentTypes: ["public.plain-text"], in: .import)
+        picker.allowsMultipleSelection = false
+        
+        picker.delegate = self
+        return picker
+    }
+
+    var rootVC: UIViewController
+    public var didPickDocument: ((URL) -> Void)?
+    
+    internal init(rootVC: UIViewController) {
+        self.rootVC = rootVC
+    }
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else { return }
+        
+        self.didPickDocument?(url)
+    }
+    
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        isPresented = false
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+struct Picker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(documentTypes: ["public.plain-text"], in: .import)
+        picker.delegate = context.coordinator
+        
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> PickerCoordinator {
+        PickerCoordinator(picker: self)
+    }
+    
+    public mutating func onPickDocument(_ didPickDocument: @escaping (URL) -> Void) {
+        self.didPickDocument = didPickDocument
+    }
+    fileprivate var didPickDocument: ((URL) -> Void)?
+    
+    class PickerCoordinator: NSObject, UIDocumentPickerDelegate {
+        internal init(picker: Picker) {
+            self.picker = picker
+        }
+        
+        var picker: Picker
+        
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            
+            picker.didPickDocument?(url)
+        }
+    }
+}
+
+
+struct Picker_Previews: PreviewProvider {
+    static var previews: some View {
+//        Picker()
+        EmptyView()
+    }
+}
